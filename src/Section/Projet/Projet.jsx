19b9f5a402181306c126dc663f905354b1c26609
nafis585 <princetoure@gmail.com>
2025-02-15 16:45:07 +0000

    Update project details and images in Projets and Projet components; enhance layout with new CSS styles

--- src/Section/Projet/Projet.jsx
+++ src/Section/Projet/Projet.jsx
@@ -8,17 +8,48 @@ const Projet = () => {
         <div className="projet-container">
           <div className="projet-container-item">
             <div className="projet-img">
-              <img src="src/img/apropos.jpg" alt="" />
+              <img src="src/img/ecom.png" alt="" />
             </div>
             <div className="projet-message">
-              <h3>title</h3>
+              <h3>E-commerce</h3>
               <p>
-                This is a project made to learn the latest languages by building
-                an app .
+                J&apos;ai conçu et développé un site e-commerce offrant une
+                expérience utilisateur optimale.
               </p>
-              <div className="outil">React</div>
+              <div className="outils">
+                <div className="outil">React</div>
+              </div>
               <div className="btns">
+                <a
+                  href="https://ecommerce-filtering-five.vercel.app/"
+                  className="a"
+                >
+                  Demo
+                </a>
                 <a href="#" className="a">
+                  Source
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="projet-container-item child">
+            <div className="projet-img">
+              <img src="src/img/portfo.png" alt="" />
+            </div>
+            <div className="projet-message">
+              <h3>PORTFOLIO</h3>
+              <p>
+                Ce projet a été réalisé pour mettre en valeur mes compétences et
+                services.
+              </p>
+              <div className="outils">
+                <div className="outil">HTML</div>
+                <div className="outil">CSS</div>
+                <div className="outil">JS</div>
+              </div>
+
+              <div className="btns">
+                <a href="https://nafis-portofolio.vercel.app/" className="a">
                   Demo
                 </a>
                 <a href="#" className="a">
@@ -37,28 +68,9 @@ const Projet = () => {
                 This is a project made to learn the latest languages by building
                 an app .
               </p>
-              <div className="outil">React</div>
-              <div className="btns">
-                <a href="#" className="a">
-                  Demo
-                </a>
-                <a href="#" className="a">
-                  Source
-                </a>
+              <div className="outils">
+                <div className="outil">React</div>
               </div>
-            </div>
-          </div>
-          <div className="projet-container-item child">
-            <div className="projet-img">
-              <img src="src/img/apropos.jpg" alt="" />
-            </div>
-            <div className="projet-message">
-              <h3>title</h3>
-              <p>
-                This is a project made to learn the latest languages by building
-                an app .
-              </p>
-              <div className="outil">React</div>
               <div className="btns">
                 <a href="#" className="a">
                   Demo
